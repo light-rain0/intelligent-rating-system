afa8dafc73dac6c8e078f12da80e03e69a247a70
qnxy <qnxy1997@gmail.com>
2025-03-01 01:59:52 +0800

    init project

---
+++ server-app/src/test/java/org/lr0/server/ServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lr0.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
